0fcfb613b8436142d9e25bcc4f776a81cfbf0d9d
Samuel Teixeira Dias <83990486+srTeixeiraDias@users.noreply.github.com>
2021-11-27 00:30:57 -0300

    Add files via upload

--- rodape.php
+++ rodape.php
@@ -6,7 +6,7 @@
       <p>Este exemplo de álbum é &copy; Bootstrap, mas, por favor, baixe e customize por conta própria.</p>
       <p>Novo no BuscaAi? <a href="./cadastro.php">Cadastre-se</a> </P> 
       <p> Deseja Falar conosco? <a href="./faleconosco.php">Fale Conosco</a>.</p>
-      <p> Quer saber mais sobre nós e a nossa missão? <a href="./sobrenos.php">Sobre Nós</a>.</p>
+      <p> Quer saber mais sobre nós e a nossa missão?<a href="./sobrenos.php">Sobre Nós</a>.</p>
 
 
     </div>
