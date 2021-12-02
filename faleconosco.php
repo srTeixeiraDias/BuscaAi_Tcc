b6ecd02c33ea9da4a4099eee766e847d9fbbd1dc
Samuel Dias <samuelteixeiradias008@gmail.com>
2021-12-02 17:50:24 -0300

    sobre nos e fale cnosco

--- faleconosco.php
+++ faleconosco.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,14 +8,33 @@
     <title>Fale Conosco</title>
 </head>
 <body>
-    
+<div class="card text-center">
+  <div class="card-header">
+    Destaque
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Entre em contato via:</h5>
+    <p class="card-text"></p>
+  </div>
+  <div class="container px-4">
+  <div class="row gx-5">
+    <div class="col">
+     <div class="p-3 border bg-light">buscaiai@hotmail.com</div>
+    </div>
+    <div class="col">
+      <div class="p-3 border bg-light">(55) 11 4002-8922</div>
+      <p> </p>
+    </div>
+  </div>
+</div>
+</div>
 <nav class="navbar navbar-expand-lg navbar navbar-dark fixed-top bg-dark">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="#"><img src="imagens/buscaaiwhite.png" height="22,5" width="117"></a>
+      <a class="navbar-brand" href="./principal.php"><img src="imagens/buscaaiwhite.png" height="22,5" width="117"></a>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link " aria-current="page" href="./principal.php">Principal</a>
--- sobrenos.php
+++ sobrenos.php
@@ -15,7 +15,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="#"><img src="imagens/buscaaiwhite.png" height="22,5" width="117"></a>
+      <a class="navbar-brand" href="./principal.php"><img src="imagens/buscaaiwhite.png" height="22,5" width="117"></a>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link " aria-current="page" href="./principal.php">Principal</a>
@@ -39,10 +39,28 @@
   <div class="card-header">
     Destaque
   </div>
+  <p></p>
   <div class="card-body">
-    <h5 class="card-title">Título especial</h5>
-    <p class="card-text">Com suporte a texto embaixo, que funciona como uma introdução a um conteúdo adicional.</p>
-    <a href="#" class="btn btn-primary">Visitar</a>
+  <h5 class="card-title">Busca Ai</h5>
+  <div id="cadastro">
+  <h5>O inicio</h5>
+
+Em meio a uma pandemia mundial no ano de 2020, pequenos comércios que não conseguiam pagar uma plataforma de vendas ou um sistema proprio de venda
+, foram os mais afetados tendo ate que baixar as portas. O BuscaAi foi projetado para atender o seu estabelecimento que precisa de ajuda para alavancar suas vendas, 
+conseguindo divulgar sua loja e seus produtos de maneira fácil através do nosso site e aplicativo para Android e IOS, facilitando o acesso do consumidor ao item desejado.
+<p></p>
+<h5>Missão</h5>
+<p>Chegar a onde nenhuma outra empresa chegou e revolucionar o mercado com um aplicativo básico de vendas e compras para todos os tipos de produtos</p>
+
+<h5>Visão</h5>
+<p>Nossa empresa preza muito os nossos consumidores e por isso desenvolvemos um aplicativo com uma interface de fácil acesso para todos os tipos de pessoas. Achamos que desse jeito podemos chamar ainda mais pessoas para o nosso aplicativo.</p>
+
+<h5>Valores</h5>
+
+    <p> Priorizamos suporte ao cliente e sempre tentamos ajudar em suas dúvidas ou dificuldades para usar o nosso app.
+Jamais iremos desrespeitar algum cliente ou deixar-nos sem alguma resposta em seu problema</p>
+<p>.</p>
+</div>
   </div>
 
 </div>
